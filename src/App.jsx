e619d985e5a6864498bbbe51fcc41d9da4a4d151
Debasish <ddebnath241@gmail.com>
2024-02-07 21:14:48 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./component/Navbar";
+import "./App.css";
+import Hero from "./component/Hero";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
